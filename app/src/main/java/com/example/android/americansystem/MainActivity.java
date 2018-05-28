21e64416a83b89a564c49b00dc6b435b2eb3e1c3
aliadil <aliadilg@Gmail.com>
2018-05-28 07:44:28 +0500

    Added functionality to application and UI improvement.

--- app/src/main/java/com/example/android/americansystem/MainActivity.java
+++ app/src/main/java/com/example/android/americansystem/MainActivity.java
@@ -15,37 +15,131 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
+    int total=0;
     int quantity = 1;
-    public void submitOrder(View view) {
+    int cutQuantity= 1;
+    int drinkQuantity=1;
+    int friesQuantity=1;
+    int divideQuantity=1;
 
-        displayPrice(quantity * 20);
-    }
-    public void divide(View view) {
+    //TEA FULL IN DE
 
-        divideTotal((quantity * 20)/quantity);
-    }
-
-    public void increment(View view) {
+    public void incrementTeaFull(View view) {
         quantity = quantity + 1;
-        display(quantity);
+        displayTeaFull(quantity);
     }
 
-    public void decrement(View view) {
+    public void decrementTeaFull(View view) {
         if (quantity >= 1)
             quantity = quantity - 1;
-        display(quantity);
+        displayTeaFull(quantity);
 
+    }
+
+    //TEA CUT IN DE
+
+    public void incrementTeaCut(View view) {
+        cutQuantity = cutQuantity + 1;
+        displayCut(cutQuantity);
+    }
+
+    public void decrementTeaCut(View view) {
+        if (cutQuantity >= 1)
+            cutQuantity = cutQuantity - 1;
+        displayCut(cutQuantity);
+
+    }
+
+    //Cold drinks IN DE
+    public void incrementColdDrinks(View view) {
+        drinkQuantity = drinkQuantity + 1;
+        displayDrink(drinkQuantity);
+    }
+
+    public void decrementColdDrinks(View view) {
+        if (drinkQuantity >= 1)
+            drinkQuantity = drinkQuantity - 1;
+        displayDrink(drinkQuantity);
+
+    }
+
+    //Fries IN DE
+    public void incrementFries(View view) {
+        friesQuantity = friesQuantity + 1;
+        displayFries(friesQuantity);
+    }
+
+    public void decrementFries(View view) {
+        if (friesQuantity >= 1)
+            friesQuantity = friesQuantity - 1;
+        displayFries(friesQuantity);
+
+    }
+
+
+    ///Divide Between IN DE
+    public void incrementDivide(View view) {
+        divideQuantity = divideQuantity + 1;
+        displayDivide(divideQuantity);
+    }
+
+    public void decrementDivide(View view) {
+        if (divideQuantity >= 2)
+            divideQuantity = divideQuantity - 1;
+        displayDivide(divideQuantity);
+
+    }
+
+    // This method calculate the total amount
+    public void submitOrder(View view) {
+
+        total= (quantity*30)+(cutQuantity*20)+(drinkQuantity*30)+(friesQuantity*50);
+        displayPrice(total);
+    }
+
+    //this method divide total amount
+    public void divide(View view) {
+
+        divideTotal(total/divideQuantity);
+    }
+    /**
+     * This method displays the given quantity value on the screen.
+     */
+    private void displayTeaFull(int number) {
+        TextView quantityTextView = (TextView) findViewById(R.id.teaFull_text_view);
+        quantityTextView.setText("" + number);
+        }
+    /**
+     * This method displays the given quantity value on the screen.
+     */
+    private void displayCut(int number) {
+        TextView quantityTextView = (TextView) findViewById(R.id.teaCut_text_view);
+        quantityTextView.setText("" + number);
     }
 
     /**
      * This method displays the given quantity value on the screen.
      */
-    private void display(int number) {
-        TextView quantityTextView = (TextView) findViewById(R.id.quantity_text_view);
+    private void displayDrink(int number) {
+        TextView quantityTextView = (TextView) findViewById(R.id.drinks_text_view);
         quantityTextView.setText("" + number);
-
-
     }
+
+    /**
+     * This method displays the given quantity value on the screen.
+     */
+    private void displayFries(int number) {
+        TextView quantityTextView = (TextView) findViewById(R.id.fries_text_view);
+        quantityTextView.setText("" + number);
+    }
+    /**
+     * This method displays the given quantity value on the screen.
+     */
+    private void displayDivide(int number) {
+        TextView quantityTextView = (TextView) findViewById(R.id.divide_text_view);
+        quantityTextView.setText("" + number);
+    }
+
 
     /**
      * This method displays the given price on the screen.@+id/divide_total
